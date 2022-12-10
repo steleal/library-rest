ed50594ebf2c3c242b98ad672bed693d8731e915
Alexey Shtele <aleksey.shtele@rt.ru>
2022-12-10 13:53:22 +0400

    Change base controller

--- src/main/java/com/github/piranha887/library/controllers/BaseController.java
+++ src/main/java/com/github/piranha887/library/controllers/BaseController.java
@@ -1,29 +1,17 @@
 package com.github.piranha887.library.controllers;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 import java.util.List;
 
 abstract class BaseController<E> {
 
-    @GetMapping
     abstract public List<E> getAll();
 
-        @GetMapping(path = "/{id}")
-    abstract public E getById(@PathVariable(value = "id") long id);
+    abstract public E getById(long id);
 
-    @PostMapping
-    abstract public long add(@RequestBody E dto);
+    abstract public long add(E dto);
 
-    @PutMapping("/{id}")
-    abstract public void update(@PathVariable(value = "id") long id, @RequestBody E dto) ;
+    abstract public void update(long id, E dto);
 
-    @DeleteMapping(path = "/{id}")
-    abstract public void delete(@PathVariable(value = "id") long id);
+    abstract public void delete(long id);
 
 }
